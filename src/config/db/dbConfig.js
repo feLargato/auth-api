d69d0c2b01ec3a9f5789e219ae1974c3c2d2c3c8
fernando.rezende <luis.santos@tuntscorp.com>
2022-04-08 18:31:56 -0300

    config

--- src/config/db/dbConfig.js
+++ src/config/db/dbConfig.js
@@ -6,6 +6,12 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     port: DB_PORT,
     dialect: "postgres",
     quoteIdentifier: false,
+    dialectOptions: {
+      ssl: {
+          require:true,
+          rejectUnauthorized: false
+      }
+    },
     define: {
         syncOnAssociation: true,
         timestamps: false,
